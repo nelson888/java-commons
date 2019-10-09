9d5a7df06d81f7e5a0e8ab2f51b5dca7f1dc3e98
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2019-10-09 17:31:15 +0200

    added unit test

---
+++ collection-utils/src/test/java/com/tambaps/utils/collectionuils/GridTest.java
@@ -0,0 +1,43 @@
+package com.tambaps.utils.collectionuils;
+
+
+import com.tambapps.utils.collectionutils.Grid;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GridTest {
+
+  @Test
+  public void rowColumnTests() {
+    rowColumnTest(new Grid<>(3, 2));
+    rowColumnTest(new Grid<>(10, 10));
+  }
+
+  private void rowColumnTest(Grid<Integer> array) {
+    int M = array.getM(), N = array.getN();
+
+    for (int j = 0; j < N; j++) {
+      for (int i = 0; i < M; i++) {
+        array.set(i, j, i * array.getM() + j);
+      }
+    }
+
+    for (int j = 0; j < N; j++) {
+      Grid.Vector<Integer> column = array.getColumn(j);
+      for (int i = 0; i < M; i++) {
+        assertEquals("Should be equal", i * array.getM() + j, (int) column.getAt(i));
+      }
+    }
+
+    for (int i = 0; i < M; i++) {
+      Grid.Vector<Integer> row = array.getRow(i);
+      for (int j = 0; j < N; j++) {
+        assertEquals("Should be equal", i * array.getM() + j, (int) row.getAt(j));
+      }
+    }
+  }
+
+
+
+}
