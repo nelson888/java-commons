1612aacc76b55c2ade33b0bf779107502a9352d7
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2019-11-08 16:25:30 +0100

    [IO] added copy method

--- io-utils/src/main/java/com/tambapps/utils/ioutils/IOUtils.java
+++ io-utils/src/main/java/com/tambapps/utils/ioutils/IOUtils.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.io.OutputStream;
 import java.io.Reader;
 
 public final class IOUtils {
@@ -41,6 +42,18 @@ public final class IOUtils {
     try (LineNumberReader lnReader = new LineNumberReader(reader)) {
       while (lnReader.skip(Long.MAX_VALUE) > 0);
       return lnReader.getLineNumber();
+    }
+  }
+
+  public static void copy(InputStream in, OutputStream out) throws IOException {
+    copy(in, out, 1024);
+  }
+
+  public static void copy(InputStream in, OutputStream out, int bufferSize) throws IOException {
+    byte[] buffer = new byte[bufferSize];
+    int len;
+    while ((len = in.read(buffer)) != -1) {
+      out.write(buffer, 0, len);
     }
   }
 }
